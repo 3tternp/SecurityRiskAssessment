75775af2094848e0ddc70ebc968271ebf4215754
astra <26215963+3tternp@users.noreply.github.com>
2025-03-17 16:12:35 +0545

    Update widgets.py

--- accounts/widgets.py
+++ accounts/widgets.py
@@ -1,21 +1,32 @@
 from django.forms import RadioSelect, CheckboxSelectMultiple
 
-
 class RadioSelectButtonGroup(RadioSelect):
     """
-    This widget renders a Bootstrap 4 set of buttons horizontally instead of typical radio buttons.
+    This widget renders a Bootstrap 5 set of buttons horizontally instead of typical radio buttons.
 
     Much more mobile friendly.
     """
 
     template_name = "widgets/radio_select_button_group.html"
 
+    def __init__(self, attrs=None, choices=()):
+        default_attrs = {'class': 'btn-check'}
+        if attrs:
+            default_attrs.update(attrs)
+        super().__init__(default_attrs, choices)
+
 class CheckboxInputGroup(CheckboxSelectMultiple):
     """
-    This widget renders a Bootstrap 4 set of buttons horizontally instead of typical radio buttons.
+    This widget renders a Bootstrap 5 set of checkboxes as buttons horizontally.
 
     Much more mobile friendly.
     """
 
     template_name = "widgets/checkbox_input_group.html"
+
+    def __init__(self, attrs=None, choices=()):
+        default_attrs = {'class': 'btn-check'}
+        if attrs:
+            default_attrs.update(attrs)
+        super().__init__(default_attrs, choices)
 
