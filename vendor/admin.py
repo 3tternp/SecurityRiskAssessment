11ebf5967ceb4d0e78362db7cc75b032e9c563d2
astra <26215963+3tternp@users.noreply.github.com>
2025-03-17 15:35:45 +0545

    Update admin.py

--- vendor/admin.py
+++ vendor/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
+from .models import Vendor, UserVendor, UserTypeVendor
 
-from .models import *
+# Customize the Vendor admin view
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'website', 'exstContract', 'slug')  # Displayed columns in the list view
+    search_fields = ('name', 'website', 'address')  # Searchable fields
+    list_filter = ('exstContract',)  # Filter options in the sidebar
 
-# Register your models here.
-admin.site.register(Vendor)
-admin.site.register(UserVendor)
-admin.site.register(UserTypeVendor)
+class UserVendorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'vendor', 'userType', 'phone')  # Displayed columns in the list view
+    search_fields = ('user__fname', 'user__lname', 'phone')  # Searchable fields, including user's first/last name
+    list_filter = ('userType',)  # Filter options in the sidebar
+    raw_id_fields = ('user',)  # Improves performance when selecting a user (especially with large databases)
+
+class UserTypeVendorAdmin(admin.ModelAdmin):
+    list_display = ('name',)  # Displayed columns in the list view
+    search_fields = ('name',)  # Searchable fields
+
+# Register the models with their respective custom admin views
+admin.site.register(Vendor, VendorAdmin)
+admin.site.register(UserVendor, UserVendorAdmin)
+admin.site.register(UserTypeVendor, UserTypeVendorAdmin)
